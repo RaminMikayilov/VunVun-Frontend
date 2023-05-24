f1786df72f77324731309197586bce8d602e6f27
RaminMikayilov <raminmikayilov2003@gmail.com>
2023-05-24 12:09:48 +0400

    Add LogIn and SignUp component

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,26 @@
-import { BrowserRouter as Router , Routes , Route } from "react-router-dom"
-import Homepage from "./pages/Homepage"
-import Error from "./pages/Error"
-import Navbar from './components/main/Navbar';
-import Footer from './components/main/Footer';
-import BeOwner from './pages/BeOwner'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Homepage from "./pages/Homepage";
+import Error from "./pages/Error";
+import Navbar from "./components/main/Navbar";
+import Footer from "./components/main/Footer";
+import BeOwner from "./pages/BeOwner";
 import CarDetails from "./components/main/CarDetails";
 function App() {
-
   return (
     <div className="App">
-      <Router> 
+      <Router>
         <Navbar />
         <Routes>
-          <Route path="/" element={<Homepage />}/>
-          <Route path="/elanver" element={<BeOwner />}/>
-          <Route path="/cars/:carId" element={<CarDetails />} /> 
-          <Route path="*" element={<Error />}/>
+          <Route path="/" element={<Homepage />} />
+          <Route path="/elanver" element={<BeOwner />} />
+          <Route path="/avtomobiller" element={<h1>All Cars</h1>} />
+          <Route path="/avtomobiller/:carId" element={<CarDetails />} />
+          <Route path="*" element={<Error />} />
         </Routes>
         <Footer />
       </Router>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
